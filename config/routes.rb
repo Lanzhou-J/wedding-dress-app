039d8f126b7a8fd7bea7826bf09cdfc7ff3bfc70
Lanzhou Jiang <lzjiang99@gmail.com>
2020-05-13 20:22:35 +1000

    formated dresses index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :dresses
   root to: "pages#home"
+  get '/user' => 'dresses#index', :as => :user_root
 end
