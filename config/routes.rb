14fe472a2c5093ef14a043925293298c4f42edcf
Lanzhou Jiang <lzjiang99@gmail.com>
2020-05-14 14:36:31 +1000

    add Create and show function for shops

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :dresses
+  resources :shops
   root to: "pages#home"
   get '/user' => 'dresses#index', :as => :user_root
 end
