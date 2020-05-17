dfa127abc6464ceb696d2eadc01e0d05544fde19
Lanzhou Jiang <lzjiang99@gmail.com>
2020-05-17 17:39:10 +1000

    Add buy now/ pay with card function(stripe) in dress show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   resources :dresses
   resources :shops
   resources :carts
+
   root to: "pages#home"
   get '/introduction', to: 'pages#introduction', as: 'introduction'
+
   get '/user' => 'dresses#index', :as => :user_root
+  get '/charges/new', to: 'charges#new', as: 'new_charge'
+  post '/buynow', to: 'charges#buy_now', as: 'buy_now'
+  post '/buycart', to: 'charges#buy_cart', as: 'buy_cart'
 end
