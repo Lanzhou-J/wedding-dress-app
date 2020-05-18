7ef46f17a194b7abdd5886710fc6dbe8cbae3b6c
Lanzhou Jiang <lzjiang99@gmail.com>
2020-05-18 13:54:46 +1000

    accessing dresses through active record instead of passing through params

--- app/javascript/packs/stripe.js
+++ app/javascript/packs/stripe.js
@@ -1,14 +1,14 @@
-//  import '@stripe/stripe-js';
+ import '@stripe/stripe-js';
 
-//  const button = document.getElementById("stripe")
-//  button.addEventListener("click", (e) => {
-//    const id = location.pathname.split("/")[2]
-//    fetch(`/payments/session?id=${id}`)
-//    .then((res) => res.json())
-//    .then((data) => {
-//      const stripe = Stripe(data.stripe_public_key);
-//      stripe.redirectToCheckout({
-//        sessionId: data.id
-//      })
-//    })
-//  })
+ const button = document.getElementById("stripe")
+ button.addEventListener("click", (e) => {
+  // making a GET request through javascript   
+   fetch(`/payments/session`)
+   .then((res) => res.json())
+   .then((data) => {
+     const stripe = Stripe(data.stripe_public_key);
+     stripe.redirectToCheckout({
+       sessionId: data.id
+     })
+   })
+ })
